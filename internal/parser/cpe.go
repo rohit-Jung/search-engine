74a61fd34eb4a937a5dcd48ab7e5e99dbf349300
rohitJung <rokshh28@gmail.com>
2026-04-26 19:27:30 +0545

    refactor: separate things out

--- internal/parser/cpe.go
+++ internal/parser/cpe.go
@@ -1,2 +1,24 @@
 // Package parser - Here we have Cpe Parsing logic
 package parser
+
+import "strings"
+
+type CPEDetails struct {
+	Part    string
+	Vendor  string
+	Product string
+	Version string
+}
+
+// GetCPEDetails - gives out splitted CPE
+// CPE - Common Platform Enumeration
+// Format - cpe:2.3:<part>:<vendor>:<product>:<version>:<update>:<edition>:<language>:<sw_edition>:<target_sw>:<target_hw>:<other>
+func (c *CPEMatch) GetCPEDetails() *CPEDetails {
+	parts := strings.Split(c.Criteria, ":")
+	return &CPEDetails{
+		Part:    parts[2],
+		Vendor:  parts[3],
+		Product: parts[4],
+		Version: parts[5],
+	}
+}
--- internal/parser/nvd.go
+++ internal/parser/nvd.go
@@ -1,1 +1,49 @@
 package parser
+
+import "strings"
+
+// NVDResponse -final api response
+type NVDResponse struct {
+	ResultsPerPage  int             `json:"resultsPerPage"`
+	StartIndex      int             `json:"startIndex"`
+	TotalResults    int             `json:"totalResults"`
+	Format          string          `json:"format"`
+	Version         string          `json:"version"`
+	Timestamp       string          `json:"timestamp"`
+	Vulnerabilities []Vulnerability `json:"vulnerabilities"`
+}
+
+// GetCVSSScore - Since CVSS can have two versions
+func (c *CVE) GetCVSSScore() float64 {
+	if len(c.Metrics.CVSSMetricV31) > 0 {
+		return c.Metrics.CVSSMetricV31[0].CVSSData.BaseScore
+	}
+	if len(c.Metrics.CVSSMetricV2) > 0 {
+		return c.Metrics.CVSSMetricV2[0].CVSSData.BaseScore
+	}
+	return 0.0
+}
+
+func (c *CVE) ShouldIndex() bool {
+	// are rejected CVE
+	if strings.ToLower(c.VulnStatus) == "rejected" {
+		return false
+	}
+
+	if len(c.Descriptions) > 0 {
+		if strings.HasPrefix(c.Descriptions[0].Value, "Rejected Reason: ") {
+			return false
+		}
+	}
+
+	if c.GetCVSSScore() == 0.0 {
+		return false
+	}
+
+	// if no Configurations : CPE then don't
+	if len(c.Configurations) == 0 {
+		return false
+	}
+
+	return true
+}
