a99760734c693dc60a5423f330cacb84373c448f
rohitJung <rokshh28@gmail.com>
2026-04-26 19:27:57 +0545

    refactor: split based on non-alphanumeric

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,7 +1,10 @@
 // Package tokenizer - exposes a function to tokenize
 package tokenizer
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 var stopwords = map[string]bool{
 	"a":   true,
@@ -13,21 +16,42 @@ func Tokenize(doc string) []string {
 	// lowercasing
 	doc = strings.ToLower(doc)
 
-	// TODO: currently it's spliting on whitespace only
-	parts := strings.Split(doc, " ")
+	// NAIVE: currently it's spliting on whitespace only
+	// parts := strings.Split(doc, " ")
+
+	// split on anything thats non alphanumeric
+	words := strings.FieldsFunc(doc, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsDigit(r)
+	})
 
 	var tokens []string
-	for _, w := range parts {
+	for _, w := range words {
 		// removal of stop words
 		if stopwords[w] {
 			continue
 		}
 
+		// this may be a noise
+		if len(w) < 3 {
+			continue
+		}
+
 		// cleanup puncations and all
+		// stemmed := Stem(w)
 
 		// TODO: stem the words
 		tokens = append(tokens, w)
 	}
 
 	return tokens
+}
+
+// TermFreqToken TF calculation
+func TermFreqToken(tokens []string) map[string]int {
+	freq := make(map[string]int)
+	for _, w := range tokens {
+		// cause zero value of int is 0 so
+		freq[w]++
+	}
+	return freq
 }
